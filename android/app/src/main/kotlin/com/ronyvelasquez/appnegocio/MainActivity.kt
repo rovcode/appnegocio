971672be77ecd74a058348ac32c88ea13dd0eb47
Rony Velásquez <velasquezcordovar@gmail.com>
2022-04-25 16:51:57 -0500

    Cambio en MainActivity.kt

--- android/app/src/main/kotlin/com/ronyvelasquez/appnegocio/MainActivity.kt
+++ android/app/src/main/kotlin/com/ronyvelasquez/appnegocio/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.ronyvelasquez.appnegocio
 
-import io.flutter.embedding.android.FlutterActivity
+/*import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-}
+}*/
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rustica App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Rustica App'),
     );
   }
 }
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Aquí el contenido:',
             ),
             Text(
               '$_counter',
